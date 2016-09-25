1c4a4e66f5ecccc8902b75b0caf9b365ac917851
tanmayfadnavis <fadnavistanmay@gmail.com>
2016-09-24 23:43:05 -0700

    Add files via upload

---
+++ Display/Display.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysis
+{
+   public class Display
+    {
+
+        public void display()
+        {
+
+            Repository rep = Repository.getInstance();
+            List<Elem> table = rep.locations;
+                
+                //DISPLAY LOCATION TABLE
+
+                Console.Write("\n\n  locations table contains:");
+
+               // Console.WriteLine("repository locations are {0} \n",rep.locations);
+
+                foreach (Elem e in table)
+                {
+                    Console.Write("\n  {0,10}, {1,25}, {2,5}, {3,5}", e.type, e.name, e.begin, e.end);
+                }
+                Console.WriteLine();
+                Console.Write("\n\n  That's all folks!\n\n");
+
+                // COUNT FOR COMPLEXITY AND FUNCTION SIZES
+
+                List<Elem> temp = rep.locations;
+
+                foreach(Elem e in table)
+                {
+                    if(e.type.Equals("namespace"))
+                    {
+                        Console.WriteLine("\n {0,10},{1,25},{2,5},{3,5}", e.type, e.name, e.begin, e.end);
+                    }
+
+                    //search for the namespace for that particular class
+
+                    if(e.type.Equals("class"))
+                    {
+                        foreach(Elem tt in temp)
+                        {
+                            if(tt.type.Equals("namespace"))
+                            {
+                                if((tt.begin < e.begin) && (tt.end > e.end))
+                                {
+                                    Console.WriteLine("\n{0,10},{1,10}.{2,5},{3,5},{4,5}", e.type, tt.name, e.name, e.begin, e.end);
+                                }
+                            }
+                        }
+
+
+                    }
+                    if(e.type.Equals("function"))
+                    {
+                        int size = e.end - e.begin;
+                        int complexity=0;
+                        foreach(Elem t in temp)
+                        {
+                            if(t.type.Equals("control") && (t.begin > e.begin) && (t.end < e.end))
+                                complexity++;
+                            if (t.type.Equals("braceless") && (t.begin > e.begin) && (t.end < e.end))
+                                complexity++;
+
+                        }
+
+                        Console.WriteLine("\n Function name is{0,10} size is{1,5} complexity is{2,5}", e.name, size, complexity);
+
+                    }
+
+                }
+
+                //DISPLAY RELATIONSHIPS
+
+                List<RelElem> table2 = rep.inheritance;
+
+                List<RelElem> temp2 = rep.inheritance;
+               //Console.Write("\n\n  relationship table contains:");
+                
+                
+                foreach (RelElem e in table2)
+                {
+                    Console.Write("\n  {0,10}, {1,25},{2,20}, {3,5}, {4,5}", e.type, e.name, e.withName, e.beginRel, e.endRel);
+                }
+                Console.WriteLine();
+                Console.Write("\n\n  That's all folks!\n\n");
+                Console.ReadLine();
+                
+                //DISPLAY FINAL RELATIONSHIPS
+
+               /* foreach(Elem ee in table)
+                {
+                    if(ee.type.Equals("class"))
+                    {
+                        foreach(RelElem rr in table2)
+                        {
+                            if(rr.beginRel > ee.begin && rr.endRel < ee.end && ee.name != rr.withName && rr.type!="UsingTemp")
+                            {
+                                Console.WriteLine("\nclass {0} has {1} relationship with {2}", ee.name, rr.type, rr.withName);
+                            }
+                        }
+                    }
+                } */
+     
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Executive/Executive.cs
@@ -0,0 +1,147 @@
+﻿/////////////////////////////////////////////////////////////////////////
+// ScopeStack.cs  -  Generic stack to help with static analysis        //
+//                   Holds application specific Element type           //
+// ver 1.0                                                             //
+// Language:    C#, Visual Studio 13.0, .Net Framework 4.5             //
+// Platform:    Lenovo V570, Win 7, SP 1                              //
+// Application: Pr#2  CSE681, Fall 2014                               //
+// Author:     Tanmay Fadnavis, Syracuse University,                  //
+//                 tfadnavi@syr.edu                                  //
+/////////////////////////////////////////////////////////////////////////
+/*
+ * Package Operations
+ * ==================
+ * The Executive package via the class Executive, provides the entry point
+ * for the code analyzer. It communicates with CommandLineParser package,
+ * FileManager package,Analyzer Package,Display Package and XML processor
+ * Package. It communicates the output of one package to the other.
+ * 
+ * 
+ *
+/*
+ * Build Process
+ * =============
+ * Required Files:
+ *   Executive.cs, CmdLine.cs, FileMgr.cs,Analyzer.cs,Display.cd,XMLProcessor.cs
+ * 
+ * Compiler Command:
+ *   csc /target:exe /define:TEST_EXECUTIVE Executive.cs, CmdLine.cs, FileMgr.cs /
+ *   Analyzer.cs,Display.cd,XMLProcessor.cs
+ * 
+ * Maintenance History
+ * ===================
+ * ver 1.0 : Oct 2014
+ *   - first release
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysis
+{
+    class Executive
+    {
+        private string path;
+        private List<string> pattern=new List<string>();
+        private List<string> options=new List<string>();
+        private bool recurse;
+
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+            set
+            {
+                path = value;
+            }
+        }
+
+        public  bool Recurse
+        {
+            get
+            {
+                return recurse;
+            }
+
+            set
+            {
+                recurse = value;
+            }
+        }
+        static public string[] getFiles(string path, List<string> patterns,bool recurse)
+        {
+            FileManager fm = new FileManager();
+            foreach (string pattern in patterns)
+                fm.addPattern(pattern);
+
+            fm.findFiles(path,patterns,recurse);
+            return fm.getFiles().ToArray();
+
+        }
+
+      
+
+        static void Main(string[] args)
+        {
+            Executive e = new Executive();
+            
+            
+                try
+                {
+
+                    CommandLineParser argument = new CommandLineParser();
+
+                    //Console.WriteLine("Command Line Argument is = \n" + arg);
+                    argument.splitArgument(args, out e.path, out e.pattern, out e.options);
+
+                   // Console.WriteLine("path is {0}", e.path);
+                    if (e.path.Length == 0)
+                        e.path = "../../";
+                   
+
+                    //Console.WriteLine("File Path is {0}\nFile Pattern is {1}\nOptions are{2}", e.Path, e.pattern, e.options);
+
+                }
+
+                catch (Exception exp)
+                {
+                    Console.WriteLine("There is an error in command line {0} \n", args);
+                    Console.WriteLine("\n Error Message {0} \n \n", exp.Message);
+                }
+            
+            Console.WriteLine(e.Recurse);
+            e.Recurse=e.options.Contains("/S");
+            Console.WriteLine(e.Recurse);   
+                
+
+            
+            string[] files = Executive.getFiles(e.path,e.pattern,e.Recurse);
+
+            Analyzer ana = new Analyzer();
+
+            Display d = new Display();
+                
+           
+                 ana.doAnalysis(files);
+                 d.display();
+            
+
+             Console.WriteLine("after display");
+
+             XMLProcessor xp = new XMLProcessor();
+
+             xp.process();
+
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
